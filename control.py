a1447664745594a01f831adedbc96a2f54081a3b
RatnakumariMallavarapu <23ht1a0588@gmail.com>
2026-07-06 22:00:36 +0530

    Add files via upload

---
+++ control.py
@@ -0,0 +1,52 @@
+age = 21
+if age > 20:
+    print("eligible")
+
+    ''''
+    execution flow
+            condition true?
+                   |
+            execute the block
+     '''''
+    
+    x = 10
+
+    if  x>5:
+        print("Hello")
+
+        #if-else :what is if state becomes false
+
+      #  if condition:
+      #      statement
+       # else:
+      #  statement
+
+      #Example: even or odd
+
+    num = 10
+    if num % 2 == 0:
+        print("even number")
+    else:
+        print("odd numbers")
+
+        ''''''
+        #elif ladder
+        #miltiple conditions : multiple decisions
+
+        if condition1:
+            statement1
+        elif condition2:
+            statement2
+        else:
+            statement3
+
+            ''''''
+#task: build a student grading system
+marks = 85
+if marks >= 90:
+    print("grade a")
+elif marks >= 80:
+    print("grade b")
+    
+
+
